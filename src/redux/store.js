abc85de1cfd9cf133ff9d6dd63bc9732c654b00e
Michal Borek <borekmichal30@gmail.com>
2020-04-13 17:35:40 +0200

    Add searcher

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import {combineReducers, createStore} from 'redux';
 import initialStoreData from '../data/dataStore.js';
 import columnsReducer from '../redux/columnsRedux.js';
 import cardsReducer from '../redux/cardsRedux.js';
+import searchStringReducer from '../redux/searchStringRedux.js';
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -16,6 +17,7 @@ const initialState = {
 const reducers = {
   columns: columnsReducer,
   cards: cardsReducer,
+  searchString: searchStringReducer,
 };
 
 // add blank reducers for initial state properties without reducers
